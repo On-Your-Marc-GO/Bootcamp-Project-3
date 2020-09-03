a523efee974110f80b6e9805a7bc095b0ab3ad16
On-Your-Marc-GO <marcos.a.arguijo@gmail.com>
2020-09-02 19:03:29 -0500

    Remove files. Start from scratch.

--- project-3/server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const app = express();
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log("Listening at http://localhost:3000");
-});
